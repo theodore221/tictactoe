b0179606ae8ef2266caebee3eb5778c3fac206e2
Theodore Xavier <theodore.xavier221@gmail.com>
2024-03-08 20:12:32 +1100

    fix css for smaller viewscreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   const style =
-    value === 'X' ? 'square x' : value === 'O' ? 'square o' : 'square';
+    value === "X" ? "square x" : value === "O" ? "square o" : "square";
 
   return (
     <button onClick={onSquareClick} className={style}>
@@ -20,9 +20,9 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
     const nextSquares = squares.slice();
 
     if (xIsNext) {
-      nextSquares[i] = 'X';
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = 'O';
+      nextSquares[i] = "O";
     }
     onPlay(nextSquares);
   }
@@ -30,40 +30,42 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = 'Winner: ' + winner;
+    status = "Winner: " + winner;
   } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
-  let moveInfo = 'You are at move # ' + (currentMove + 1);
+  let moveInfo = "You are at move # " + (currentMove + 1);
   console.log(winner);
-  let stateStyle = winner ? 'status ' + winner : 'status';
+  let stateStyle = winner ? "status " + winner : "status";
   stateStyle = stateStyle.toLowerCase();
   console.log(stateStyle);
 
   return (
     <>
       {/* TODO: Re-add the status back in within Game component */}
-      <div className={stateStyle}>
-        {status}
-        <br></br>
-        {/* {moveInfo} */}
-      </div>
-      <div className="board-grid">
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <div className="board__status">
+        <div className={stateStyle}>
+          {status}
+          <br></br>
+          {/* {moveInfo} */}
         </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <div className="board-grid">
+          <div className="board-row">
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
       </div>
     </>
@@ -114,9 +116,9 @@ export default function Game() {
     moves = history.map((squares, move) => {
       let description;
       if (move > 0) {
-        description = '#' + move + '.';
+        description = "#" + move + ".";
       } else {
-        description = 'Go to game Start';
+        description = "Go to game Start";
       }
 
       return (
@@ -138,9 +140,9 @@ export default function Game() {
   let moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = '#' + move + '.';
+      description = "#" + move + ".";
     } else {
-      description = 'Go to game Start';
+      description = "Go to game Start";
     }
 
     return (
